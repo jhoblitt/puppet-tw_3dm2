d46f665009306f1fab8eeaccca1a0e953c4cfcae
Joshua Hoblitt <jhoblitt@cpan.org>
2012-11-12 17:20:51 -0700

    remove extra whitespace

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class 3dm2::params {
 
   case $::osfamily {
     redhat: {
-      $unzip_path       = '/root/3ware'
+      $unzip_path = '/root/3ware'
     }
     default: {
       fail("Module ${module_name} is not supported on ${::operatingsystem}")

3e0d791b4b40427845bfc7ed8553b156c089516e
Joshua Hoblitt <jhoblitt@cpan.org>
2013-05-18 11:24:31 -0700

    add tw_controller_#{unit}_#{type}_drives facts
    Along with additional tw_cli output examples and a few new code comments

--- lib/facter/3ware.rb
+++ lib/facter/3ware.rb
@@ -7,16 +7,6 @@ Facter.add(:tw_cli) do
   end
 end
 
-Facter.add(:tw_controllers) do
-  confine :kernel => "Linux"
-  setcode do
-    tw_cli = Facter.value('tw_cli')
-    unless tw_cli
-       return nil
-    end
-
-    output = Facter::Util::Resolution.exec("#{tw_cli} show")
-
 # example output 1:
 #
 # # tw_cli show version
@@ -43,6 +33,32 @@ Facter.add(:tw_controllers) do
 # c0    9750-8i      7         7        3       0       1       1      OK       
 # 
 
+# example output 3:
+#
+# # tw_cli show version
+# CLI Version = 2.00.11.016
+# API Version = 2.08.00.017
+# 
+# # tw_cli show
+# 
+# Ctl   Model        (V)Ports  Drives   Units   NotOpt  RRate   VRate  BBU
+# ------------------------------------------------------------------------
+# c0    9650SE-8LPML 8         8        3       0       1       1      OK       
+# 
+
+Facter.add(:tw_controllers) do
+  confine :kernel => "Linux"
+  setcode do
+    # we can't do anything without the tw_cli utility
+    tw_cli = Facter.value('tw_cli')
+    unless tw_cli
+      return nil
+    end
+
+    output = Facter::Util::Resolution.exec("#{tw_cli} show")
+
+    # parse out the device name of each controller
+    # controllers = ['c0','c1']
     controllers = []
     output.split(/\n/).each { |line|
       next if line =~ /^$/    # blank line
@@ -56,10 +72,109 @@ Facter.add(:tw_controllers) do
   end
 end
 
-#if Facter.value(:kernel) == "Linux"
-#  if tw_controllers = Facter.value('tw_controllers')
-#    controllers = tw_controllers.split(',') 
-#    controllers.each{ |unit|
-#    }
-#  end
-#end
+# example output 1:
+#
+# # tw_cli show version
+# CLI Version = 2.00.11.020
+# API Version = 2.08.00.023
+# 
+# # tw_cli /c8 show drivestatus
+# 
+# VPort Status         Unit Size      Type  Phy Encl-Slot    Model
+# ------------------------------------------------------------------------------
+# p0    OK             u0   136.73 GB SAS   0   -            SEAGATE ST3146356SS 
+# p1    OK             u0   136.73 GB SAS   1   -            SEAGATE ST3146356SS 
+# p2    OK             u0   136.73 GB SAS   2   -            SEAGATE ST3146356SS 
+# p3    OK             u0   136.73 GB SAS   3   -            SEAGATE ST3146356SS 
+# p4    OK             u0   136.73 GB SAS   4   -            SEAGATE ST3146356SS 
+# p5    OK             u0   136.73 GB SAS   5   -            SEAGATE ST3146356SS 
+# p6    OK             u0   136.73 GB SAS   6   -            SEAGATE ST3146356SS 
+# p7    OK             u0   136.73 GB SAS   7   -            SEAGATE ST3146356SS 
+# 
+
+# example output 2:
+#
+# # tw_cli show version
+# CLI Version = 2.00.11.016
+# API Version = 2.08.00.017
+# 
+# # tw_cli /c0 show drivestatus
+# 
+# VPort Status         Unit Size      Type  Phy Encl-Slot    Model
+# ------------------------------------------------------------------------------
+# p0    OK             u0   136.73 GB SAS   0   -            SEAGATE ST3146356SS 
+# p1    OK             u0   136.73 GB SAS   1   -            SEAGATE ST3146356SS 
+# p2    OK             u1   136.73 GB SAS   2   -            SEAGATE ST3146356SS 
+# p3    OK             u1   136.73 GB SAS   3   -            SEAGATE ST3146356SS 
+# p4    OK             u1   136.73 GB SAS   4   -            SEAGATE ST3146356SS 
+# p5    OK             u1   136.73 GB SAS   5   -            SEAGATE ST3146356SS 
+# p6    OK             u2   136.73 GB SAS   6   -            SEAGATE ST3146356SS
+# 
+
+# example output 3:
+#
+# # tw_cli show version
+# CLI Version = 2.00.11.016
+# API Version = 2.08.00.017
+# 
+# # tw_cli /c0 show drivestatus
+# 
+# VPort Status         Unit Size      Type  Phy Encl-Slot    Model
+# ------------------------------------------------------------------------------
+# p0    OK             u0   931.51 GB SATA  0   -            ST31000524NS        
+# p1    OK             u0   931.51 GB SATA  1   -            ST31000524NS        
+# p2    OK             u1   931.51 GB SATA  2   -            ST31000524NS        
+# p3    OK             u1   931.51 GB SATA  3   -            ST31000524NS        
+# p4    SMART-FAILURE  u1   931.51 GB SATA  4   -            ST31000524NS        
+# p5    OK             u1   931.51 GB SATA  5   -            ST31000524NS        
+# p6    OK             u1   931.51 GB SATA  6   -            ST31000524NS        
+# p7    OK             u2   931.51 GB SATA  7   -            ST31000524NS        
+# 
+
+if Facter.value(:kernel) == "Linux"
+  # we can't do anything without the tw_cli utility
+  tw_cli = Facter.value('tw_cli')
+  # or a list of controllers (which we wouldn't have without a working tw_cli)
+  tw_controllers = Facter.value('tw_controllers')
+
+  if defined?(tw_cli) and defined?(tw_controllers)
+    # iterate over each controller and get the list of attached drives
+    controllers = tw_controllers.split(',') 
+    controllers.each{ |unit|
+      output = Facter::Util::Resolution.exec("#{tw_cli} /#{unit} show drivestatus")
+
+      # parse out the vport # and interface type for each drive
+      # drives = { 'sas' = ['0','1'] }
+      drives = {}
+      output.split(/\n/).each { |line|
+        next if line =~ /^$/      # blank line
+        next if line =~ /^\s+/    # white space only (may not be needed)
+        next if line =~ /^VPort+/ # header line
+        next if line =~ /^-+/     # deliminter line
+
+        fields = line.split(/\s+/)
+        vport  = fields[0]
+        type   = fields[5]
+
+        # if this is a new drive type init list of drives array
+        unless drives.has_key?(type)
+          drives[type] = []
+        end
+
+        # convert vport "p5" -> "5" as tw_cli is inconsitent about refering
+        # to disks as "pN" or just "N" and smartctl uses just N
+        drives[type].push(/^p(\d+)$/.match(vport)[1])
+      }
+
+      # create a new fact for each controller + drive type pair
+      drives.each_key{ |type| 
+        Facter.add(:"tw_controller_#{unit}_#{type}_drives") do
+          confine :kernel => "Linux"
+          setcode do
+            drives[type].sort.join(',')
+          end
+        end
+      }
+    }
+  end
+end
